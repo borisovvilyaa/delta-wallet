62ff5aa86fe50f65cf9ef361b425d30665d4dff3
borisovvilyaa <borisovvilyaa@gmail.com>
2023-08-25 13:46:09 +0300

    fix commit

--- main.go
+++ main.go
@@ -1,23 +1,23 @@
-// package main
+package main
 
-// import (
-// 	"delta_wallet/handlers"
+import (
+	"delta_wallet/handlers"
 
-// 	"github.com/gin-gonic/gin"
-// )
+	"github.com/gin-gonic/gin"
+)
 
-// func main() {
-// 	router := gin.Default()
+func main() {
+	router := gin.Default()
 
-// 	router.LoadHTMLGlob("templates/**/*")
-// 	router.Static("/static", "./static")
+	router.LoadHTMLGlob("templates/**/*")
+	router.Static("/static", "./static")
 
-// 	router.GET("/", handlers.Index)
-// 	router.GET("/home", handlers.Home)
-// 	router.GET("/receive", handlers.Receive)
-// 	router.GET("/send", handlers.Send)
-// 	router.GET("/login", handlers.Login)
-// 	router.GET("/create-wallet", handlers.CreateWallet)
+	router.GET("/", handlers.Index)
+	router.GET("/home", handlers.Home)
+	router.GET("/receive", handlers.Receive)
+	router.GET("/send", handlers.Send)
+	router.GET("/login", handlers.Login)
+	router.GET("/create-wallet", handlers.CreateWallet)
 
-// 	router.Run(":8080")
-// }
+	router.Run(":8080")
+}
